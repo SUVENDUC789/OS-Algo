1fa8cd43670c85dba51f6e47fc930cbffb918f1e
Suvendu <suvendu.chok@gmail.com>
2023-03-31 14:42:06 +0530

    FCFS ready

---
+++ Disk-Scheduling/FCFS/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    private static int abs(int n) {
+        if (n < 0)
+            n = n * (-1);
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int process[] = { 98, 183, 37, 122, 14, 124, 65, 67 };
+        // int total_track = 200;
+        int READ_WRITE_HEAD = 53;
+
+        double total_head_movemenet = 0;
+        // double avg_head_movement = 0;
+
+        for (int i = 0; i < process.length - 1; i++) {
+            total_head_movemenet += (abs(process[i] - process[i + 1]));
+        }
+        total_head_movemenet += abs(process[0] - READ_WRITE_HEAD);
+
+        System.out.println("The Total head movement : " + ((int) total_head_movemenet));
+        System.out.println("The Average head movement : " + (total_head_movemenet / (process.length)));
+    }
+
+}
